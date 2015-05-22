80b62d92dd063bf9ef531ae04441a2134da24829
Nikita <nmatyukov.dev@gmail.com>
2015-05-22 13:39:04 +0300

    Rspec fail for User model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should respond_to(:email) }
+  it { should respond_to(:encrypted_password) }
+
+  it { should have_many(:files) }
+
 end
