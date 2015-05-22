c63c9681501d0cac46d7b478d6a9c9a954cf0941
Nikita <nmatyukov.dev@gmail.com>
2015-05-22 18:27:51 +0300

    Base config for plupload.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,15 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/buttons.png )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/buttons-disabled.png )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/transp50.png )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/backgrounds.gif )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/delete.gif )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/error.gif )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/done.gif )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/throbber.gif )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/loading.gif )
+Rails.application.config.assets.precompile += %w( jquery.plupload.queue/plupload.gif )
+Rails.application.config.assets.precompile += %w( Moxie.swf )
+Rails.application.config.assets.precompile += %w( Moxie.xap )
